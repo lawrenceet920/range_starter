9339237dc75c8107339d8b5d0580995799abe1f0
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-11-06 13:31:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,38 +3,42 @@
 # Range()
 
 # Counting to twenty 
-for num in range(1, 21):
-    print(num)
-else:
-    print('')
+# for num in range(1, 21):
+#     print(num)
+# else:
+#     print('')
 
 # One-Million
 # for num in range(1, 1000001):
 #     print(num)
 
 # Summing a Million
-operations = [min, max, sum]
-for x in operations:
-    print(x(range(1, 1000001)))
-else:
-    print('')
+# operations = [min, max, sum]
+# for x in operations:
+#     print(x(range(1, 11)))
+# else:
+#     print('')
 
 # print(min(range(1, 1000001)))
 # print(max(range(1, 1000001)))
 # print(sum(range(1, 1000001)))
     
 # Odd numbers
+# for num in range(1, 21, 2):
+#     print(num)
+# else:
+#     print('')
+
 for num in range(1, 21, 2):
-    print(num)
-else:
-    print('')
+    if num % 2 == 1:
+        print(num)
 
 # Threes
-for num in range(3, 31, 3):
-    print(num)
-else:
-    print('')
+# for num in range(3, 31, 3):
+#     print(num)
+# else:
+#     print('')
 
 # Cubes
-for num in range(1, 11):
-    print(num**3)
+# for num in range(1, 11):
+#     print(num**3)
